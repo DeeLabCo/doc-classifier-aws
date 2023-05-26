406edabe74450437c868e9f5d23074d2992cca04
Rafael Ceron Espinosa <rafaelceronespinosa@Rafaels-MacBook-Pro-2.local>
2023-05-26 16:35:29 -0500

    download classification output from s3

---
+++ src/functions/classify-extractor/app.js
@@ -0,0 +1,8 @@
+
+
+const checkClassificationOutput = (event, context) => {
+  // download the file from s3 and persist in tmp folder
+};
+
+
+exports.handler = checkClassificationOutput;
---
+++ src/functions/classify-extractor/services/s3_services.js
@@ -0,0 +1,35 @@
+
+// download to tmp Lambda
+const fs = require('fs');
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+
+const client = new S3Client({ region: "us-east-1" });
+
+const persistStream = (filePath, stream) => {
+  return new Promise ((resolve, reject) => {
+    stream.pipe(fs.createWriteStream(filePath))
+      .on('error', (err) => {
+        console.info('Error while persisting stream');
+        reject(err);
+      }).on('close', () => {
+        console.info('Stream persisted in tmp');
+        resolve()
+      })
+  });
+};
+
+const downloadFileAndStoreInTmp = async (bucketName, objectKey) => {
+  const filePath = "/tmp/" + objectKey;
+  const command = {
+    Bucket: bucketName,
+    Key: objectKey
+  };
+  const getObjectCommand = new GetObjectCommand(command);
+  const fileData = await  client.send(getObjectCommand);
+  const bodyStream = fileData.Body;
+  await persistStream(filePath, bodyStream);
+};
+
+module.exports = {
+  downloadFileAndStoreInTmp
+}

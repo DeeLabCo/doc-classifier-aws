10879f6f3c0d9ad86f4b01f43b747ebd04cda668
bitjep <julian.plazas@thebitbang.company>
2023-06-02 02:49:25 -0500

    [FIX] Fixed problem with streaming compressed files.

--- src/functions/classify-extractor/services/untargz_classification.js
+++ src/functions/classify-extractor/services/untargz_classification.js
@@ -4,13 +4,14 @@ const decompress = require('decompress');
 const decompressTargz = require('decompress-targz');
 
 const get_targz_outfile_json = async (file_path) => {
-    const classification_files = await decompress(file_path, {
-        plugins: [
-            decompressTargz()
-        ],
-        filter: file => path.extname(file.path) === '.out',
-        map: file => JSON.parse(file.data.toString())
-    });
+    // const classification_files = await decompress(file_path, {
+    //     plugins: [
+    //         decompressTargz()
+    //     ],
+    //     filter: file => path.extname(file.path) === '.out',
+    //     map: file => JSON.parse(file.data.toString())
+    // });
+    const classification_files = decompressTargz()(file_path);
     return classification_files;
 };
 

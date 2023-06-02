526b77e5fa304e45714e6c4826785c8aaa24487d
bitjep <julian.plazas@thebitbang.company>
2023-06-02 09:44:04 -0500

    [FIX] Result returned as string as JSON is not properly formatted.

--- src/functions/classify-extractor/services/untargz_classification.js
+++ src/functions/classify-extractor/services/untargz_classification.js
@@ -12,10 +12,12 @@ const get_targz_outfile_json = async (file_path) => {
     //     map: file => JSON.parse(file.data.toString())
     // });
     const s3_files = await decompressTargz()(file_path);
-    const classification_files = s3_files
-        .filter(file => path.extname(file.path) === '.out')
-        .map(file => JSON.parse(file.data.toString()));
-    return classification_files;
+    console.log("s3_files", s3_files);
+    const classification_files = s3_files.filter(file => path.extname(file.path) === '.out');
+    console.log("classification_files", classification_files);
+    const classification_data = classification_files.map(file => file.data.toString());
+    console.log("classification_data", classification_data);
+    return classification_data;
 };
 
 module.exports = get_targz_outfile_json;

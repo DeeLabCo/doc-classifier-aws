aadbcc3f3638bbb3a7597db1fa1c42fc8e730110
bitjep <julian.plazas@thebitbang.company>
2023-06-05 15:27:54 -0500

    [FIX] Fixed JSON parsing.

--- src/functions/classify-extractor/services/untargz_classification.js
+++ src/functions/classify-extractor/services/untargz_classification.js
@@ -1,22 +1,19 @@
 const path = require('path');
 
-const decompress = require('decompress');
+// const decompress = require('decompress');
 const decompressTargz = require('decompress-targz');
 
 const get_targz_outfile_json = async (file_path) => {
-    // const classification_files = await decompress(file_path, {
-    //     plugins: [
-    //         decompressTargz()
-    //     ],
-    //     filter: file => path.extname(file.path) === '.out',
-    //     map: file => JSON.parse(file.data.toString())
-    // });
     const s3_files = await decompressTargz()(file_path);
-    console.log("s3_files", s3_files);
+    // console.log("s3_files", s3_files);
     const classification_files = s3_files.filter(file => path.extname(file.path) === '.out');
-    console.log("classification_files", classification_files);
-    const classification_data = classification_files.map(file => file.data.toString());
-    console.log("classification_data", classification_data);
+    // console.log("classification_files", classification_files);
+    const classification_data = classification_files.map(file => {
+        let split_data = file.data.toString().split('\n');
+        split_data = split_data.filter(data => data != '');
+        return split_data.map(data => JSON.parse(data));
+    });
+    // console.log("classification_data", classification_data);
     return classification_data;
 };
 
